7b8a37bb3b97379527fe99b80197d86cd6f21689
Mat Schaffer <mat.schaffer@gmail.com>
2009-04-13 17:45:57 -0400

    Added a spec'd and functioning train model.

---
+++ iSepta Train View.dcproj/project.wdgt/train.js
@@ -0,0 +1,27 @@
+Train = function(number, line, departure_time) {
+  this.number = number;
+  this.line = line.toLowerCase();
+  this.set_departure_time(departure_time);
+};
+
+Train.prototype = {
+  set_departure_time: function(time) {
+    this.departure_time_string = time;
+    this.departure_time_object = Date.parse(time);
+  },
+  
+  departure_time: function() {
+    return this.departure_time_string;
+  },
+  
+  departed: function() {
+    return this.departure_time_object < new Date();
+  },
+  
+  set_status: function(status_string) {
+    this.status = status_string.replace("\n", '');
+    if (this.status.match(/\d/)) {
+        this.status += " late";
+    }
+  }
+};
---
+++ test/train_spec.js
@@ -0,0 +1,25 @@
+Screw.Unit(function () {
+  describe('Train', function() {
+    before(function() {
+      this.train = new Train(123, "R6", "8:51 AM");
+    });
+    
+    it("should store it's number, downcased line and departure time as given", function() {
+      expect(this.train.number).to(equal, 123);
+      expect(this.train.line).to(equal, "r6");
+      expect(this.train.departure_time()).to(equal, "8:51 AM");   
+    });
+
+    it("should know if it has already departed", function() {
+      var train = new Train(123, "R6", (1).minute().ago().toString("h:mm tt"));
+      expect(train.departed()).to(equal, true);
+      train.set_departure_time((1).minute().fromNow().toString("h:mm tt"));
+      expect(train.departed()).to(equal, false);
+    });
+    
+    it("should set status from trainview style string", function() {
+      this.train.set_status("4 mins\n");
+      expect(this.train.status).to(equal, "4 mins late");
+    });
+  });
+});
