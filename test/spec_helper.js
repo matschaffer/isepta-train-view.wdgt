7a5788294e2ea700bcb919bf1b281c7a2094ab53
Mat Schaffer <mat.schaffer@gmail.com>
2009-04-24 20:37:39 -0400

    Fleshed out signal spec helper to support timeout if signal is not triggered. Also fixed tests so the expectations actually fire correctly.

--- test/spec_helper.js
+++ test/spec_helper.js
@@ -1,15 +1,47 @@
+/**
+ * Delays function execution until the given jQuery signal is triggered.
+ *
+ * Example:
+ *   it("should wait for the signal", function(me) {
+ *     var myObject;
+ *     signal(me).when(myObject).triggers('explode', function() {
+ *       // Specifications go here
+ *     });
+ *     $(myObject).trigger('explode');
+ *   });
+ *
+ * Example:
+ *   it("should only wait 2 seconds for the signal", function(me) {
+ *     var myObject;
+ *     signal(me).within(2).when(myObject).triggers('explode', function() {
+ *       // Specifications go here
+ *     });
+ *     $(myObject).trigger('explode');
+ *   });
+ */
 Screw.Utilities.signal = function(me) {
-  return { when: function(target) {
-    return { triggers: function(signal, fn) {
-      $(target).one(signal, function() {
-        try {
-          fn(me);
-        } catch (e) {
-          me.trigger('failed', [e]);
-          return;
+  var timeout, delay = 2000;
+  return {
+    within: function(seconds) {
+      delay = seconds * 1000;
+      return this;
+    },
+    when: function(target) {
+      return { triggers: function(signal, fn) {
+        $(target).one(signal, function() {
+          clearTimeout(timeout);
+          try {
+            fn(me);
+            me.trigger('passed');
+          } catch (e) {
+            me.trigger('failed', [e]);
+          }
+        });
+        timeout = setTimeout(function() {
+            me.trigger('failed', target + " did not trigger '" + signal + "' within " + delay + "ms");
+          }, delay);
         }
-        me.trigger('passed');
-      });
-    }};
-  }};
+      };
+    }
+  };
 };
